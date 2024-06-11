14d1f5863cccd5f6dd1943e11418dec0c2b836b9
Lukas Jirikovsky <105300002+lukas2932@users.noreply.github.com>
2024-06-11 20:02:18 +0200

    Category connections

--- client/src/pages/NoteCreateForm/NoteCreateForm.jsx
+++ client/src/pages/NoteCreateForm/NoteCreateForm.jsx
@@ -1,14 +1,49 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { createNote } from "../../models/Note";
+import CategoryLink from "../CategoryList/CategoryLink";
+import { getCategories } from "../../models/Category";
 import "./NoteCreateForm.css";
 import * as React from "react";
 import Button from "@mui/material/Button";
+import { RoomPreferences } from "@mui/icons-material";
 
 export default function NoteCreateForm() {
+  const [categories, setCategories] = useState();
+  const [isLoaded, setLoaded] = useState(false);
   const [formData, setFormData] = useState();
   const [info, setInfo] = useState();
   const navigate = useNavigate();
+  
+  const load = async () => {
+    const data = await getCategories();
+    if (data.status === 500 || data.status === 404) return setLoaded(null);
+    if (data.status === 200) {
+      console.log(data.payload)
+      setCategories(data.payload);
+      setLoaded(true);
+    }
+  }
+
+  useEffect(() => {
+    load();
+  }, []);
+
+  if (isLoaded === null) {
+    console.log(1111111)
+    return (
+      <>
+      </>
+    )
+  }
+
+  if (!isLoaded) {
+    console.log(222222)
+    return (
+      <>
+      </>
+    )
+  }
 
   const postForm = async () => {
     const note = await createNote(formData);
@@ -22,6 +57,8 @@ export default function NoteCreateForm() {
   const handleChange = (e) => {
     if (e.target.value !== "Enter type") {
       console.log(e.target.name);
+      console.log(e.target.value);
+      console.log(  )
       setFormData({ ...formData, [e.target.name]: e.target.value });
     } else {
       e.target.value = "";
@@ -35,7 +72,7 @@ export default function NoteCreateForm() {
   };
 
   const redirectToSuccessPage = (id) => {
-    return navigate(`/creatednote/${id}`);
+    return navigate(`/notes`);
   };
 
   return (
@@ -68,10 +105,15 @@ export default function NoteCreateForm() {
             name="type"
             onChange={(value) => handleChange(value)}
           >
-            <option value="Enter type"></option>
-            <option value="Domácnost">Domácnost</option>
-            <option value="Škola">Škola</option>
-            <option value="Obecné">Obecné</option>
+            <option value = ""></option>
+            <option value = "Domácnost">Domácnost</option>
+            <option value = "Škola">Škola</option>
+            <option value = "Obecné">Obecné</option>
+            {
+              categories.map((category, index) => (
+                <option value={`${categories[index].category}`}><CategoryLink key={index} {...category} /></option>
+              ))
+            }
           </select>
           <input
             type="text"
