054c89bd5a3fe634ccdedb10282ad6d13faa6b72
Lukas Jirikovsky <105300002+lukas2932@users.noreply.github.com>
2024-06-13 20:41:24 +0200

    Sort of categories added

--- client/src/pages/NoteList/NoteList.jsx
+++ client/src/pages/NoteList/NoteList.jsx
@@ -5,10 +5,18 @@ import { getNotes } from "../../models/Note";
 import "./NoteList.css";
 import * as React from "react";
 import Button from "@mui/material/Button";
+import { getCategories } from "../../models/Category";
+import CategoryLink from "../CategoryList/CategoryLink";
 
 export default function NoteList() {
   const [notes, setNotes] = useState();
   const [isLoaded, setLoaded] = useState(false);
+  
+  const [categories, setCategories] = useState();
+  const [isLoadedCategories, setLoadedCategories] = useState(false);
+
+  const [filterValue, setFilterValue] = useState("");
+  const current_notes = []
 
   const load = async () => {
     const data = await getNotes();
@@ -19,9 +27,55 @@ export default function NoteList() {
     }
   };
 
+  const load_notes = (value) => {
+    notes.forEach(element => {
+      if (element.type == value.target.value){
+        console.log("True", element)
+        return(
+          <>
+            <p>DONE</p>
+            {
+              NoteLink(element)
+            }
+          </>
+        )
+      }
+    });
+  }
+  
+
   useEffect(() => {
     load();
   }, []);
+
+  const load_categories = async () => {
+    const data = await getCategories();
+    if (data.status === 500 || data.status === 404) return setLoadedCategories(null);
+    if (data.status === 200) {
+      setCategories(data.payload);
+      setLoadedCategories(true);
+    }
+  }
+
+  useEffect(() => {
+    load_categories();
+  }, []);
+
+  if (isLoadedCategories === null) {
+    console.log(1111111)
+    return (
+      <>
+      </>
+    )
+  }
+
+  if (!isLoadedCategories) {
+    console.log(222222)
+    return (
+      <>
+      </>
+    )
+  }
 
   if (isLoaded === null) {
     return (
@@ -46,9 +100,48 @@ export default function NoteList() {
     <>
     <div id="NoteList">
       <h1>Note list</h1>
-      {notes.map((note, index) => (
-        <NoteLink key={index} {...note} />
-      ))}
+      <select
+            type="text"
+            name="type"
+            onChange={(value) => {
+              setFilterValue(value)
+            }}
+          >
+            <option value = "">Všechny poznámky</option>
+            <option value = "Domácnost">Domácnost</option>
+            <option value = "Škola">Škola</option>
+            <option value = "Obecné">Obecné</option>
+            {
+              categories.map((category, index) => (
+                <option value={`${categories[index].category}`}><CategoryLink key={index} {...category} /></option>
+              ))
+            }
+          </select>
+      {
+        notes.forEach((element) => {
+          if (filterValue){
+            if (filterValue.target.value == element.type){
+              current_notes.push(element)
+            }
+          }
+        })
+      }
+      {
+        console.log("TYPE: ", typeof(filterValue))
+      }
+      {typeof(filterValue) == "object" ? console.log("VALUE: ", filterValue.target.value): null}
+      {current_notes ? 
+        current_notes.map((note, index) => (
+          <NoteLink key={index} {...note} />
+        )) : null}
+      {current_notes.length == 0 && typeof(filterValue) == "string" || filterValue && filterValue.target.value.trim() == ""? 
+        notes.map((note, index) => (
+          <NoteLink key={index} {...note} />
+        )) : null}
+      {current_notes.length == 0 && filterValue && filterValue.target.value.trim() != "" ?
+        <p>No notes found</p>
+        : null}
+      
       <Link className="backBtn" to={"/"}>
         <Button variant="outlined">Go back</Button>
       </Link>
